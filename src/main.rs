d492ca8fa2ef0e6b0664774c63e4cca8b33831c6
syihabb74 <syihabb74@gmail.com>
2026-03-29 12:11:16 +0700

    Refactor main function to include usage example for Post struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rust_oop_style::rust_style::Post;
+
 fn main() {
-    
+
+    // usage example
+    let mut post = Post::new();
+    post.add_content("Hello rust");
+    let post = post.pending_review();
+    let post = post.approve();
+    assert_eq!("Hello rust", post.show_content());
+    println!("{}", post.show_content())
 }
